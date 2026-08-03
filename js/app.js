1e82ba0425e2d116770a55f00cdd35e66c1f11af
haseeb9424 <haseeb9424@gmail.com>
2026-08-04 00:46:07 +0500

    Commit

--- js/app.js
+++ js/app.js
@@ -44,6 +44,7 @@
             tabManager: persistentMultipleTabManager()
         })
     });
+    window.db = db;
     const auth = getAuth(app);
     const googleProvider = new GoogleAuthProvider();
     googleProvider.setCustomParameters({ prompt: 'select_account' });
